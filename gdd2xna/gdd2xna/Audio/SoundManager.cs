9582c1ce78f07e45e4d57a45d2e9f073a5267f8d
Nathaniel Ford <nwf6197@rit.edu>
2014-04-16 11:46:38 -0400

    Added SoundManager.cs Not coded yet.

---
+++ gdd2xna/gdd2xna/Audio/SoundManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using System.IO;
+
+namespace gdd2xna
+{
+    class SoundManager
+    {
+        #region instance variables
+        #endregion
+
+        #region public methods
+        #endregion
+
+        #region internal
+        #endregion
+    }
+}
