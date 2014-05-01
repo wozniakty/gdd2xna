eaeaf64a2cf662abe5e73a25b23a7340da92d526
Jason <jtmac6@gmail.com>
2014-05-01 09:22:42 -0400

    started buttons

--- gdd2xna/gdd2xna/src/Buttons.cs
+++ gdd2xna/gdd2xna/src/Buttons.cs
@@ -2,10 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using TomShane.Neoforce.Controls;
+
 
 namespace gdd2xna
 {
     class Buttons
     {
+
+        private readonly Game1 game;
+
+        private readonly SoundManager soundManager;
+
+        public Buttons() { }
+
+        private void Draw(){}
+
+
     }
 }
